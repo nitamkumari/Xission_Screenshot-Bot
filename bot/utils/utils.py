df72efc9a3fda7a9c4f047a0af2e9355979de5b0
nitamkumari <115548739+nitamkumari@users.noreply.github.com>
2022-11-06 15:03:58 +0530

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -306,7 +306,7 @@ async def screenshot_fn(c, m):
         else:
             await media_msg.reply_media_group(screenshots, True)
         
-        await edit_message_text(m, text=f'Successfully completed process in {datetime.timedelta(seconds=int(time.time()-start_time))}\n\n\n\n©️ @odbots  @TGBotsZ  @InFoTelGroup')
+        await edit_message_text(m, text=f'Successfully completed process in {datetime.timedelta(seconds=int(time.time()-start_time))}\n\n\n\n©️ @XissionBots  @NoNStopMoviez4U  @NoNStopFilmz')
         c.CURRENT_PROCESSES[chat_id] -= 1
         
     except:
